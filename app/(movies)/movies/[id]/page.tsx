b00b0a9f26572cb217c9e357a1805a0da4fab9d1
honi <honi85@naver.com>
2024-08-07 21:28:50 +0900

    test for running vercel

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,20 +1,24 @@
 import { Suspense } from "react";
-import { API_URL } from "../../../(home)/page";
-import MovieInfo from "../../../components/movie-info";
-import MoiveVideos from "../../../components/movie-videos";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
+import MoiveVideos from "../../../../components/movie-videos";
 
-export default async function MovieDetail({
-  params: { id },
-}: {
+interface IParams {
   params: { id: string };
-}) {
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
+
+export default async function MovieDetail({ params: { id } }: IParams) {
   return (
     <div>
-      <h3>Movie Detail Page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <h3>Movie Detail Videos</h3>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MoiveVideos id={id} />
       </Suspense>
